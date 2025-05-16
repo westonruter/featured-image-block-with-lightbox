622c5ff2d10458e923a40e17ea9a36cd5af95caa
Weston Ruter <weston@ruter.net>
2025-05-16 15:24:48 -0700

    Initial commit

---
+++ featured-image-block-with-lightbox.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: Featured Image Block with Lightbox
+ * Plugin URI: https://github.com/westonruter/featured-image-block-with-lightbox
+ * Description: Automatically enables "Enlarge on click" (lightbox) for the Featured Image block when "Link to Post" is not enabled. This is a workaround to implement <a href="https://github.com/WordPress/gutenberg/issues/57849">gutenberg#57849</a>. There are no settings for this plugin.
+ * Requires at least: 6.5
+ * Requires PHP: 8.1
+ * Version: 0.1.0
+ * Author: Weston Ruter
+ * Author URI: https://weston.ruter.net/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Update URI: https://github.com/westonruter/featured-image-block-with-lightbox
+ * GitHub Plugin URI: https://github.com/westonruter/featured-image-block-with-lightbox
+ *
+ * @package FeaturedImageBlockWithLightbox
+ */
+
+namespace FeaturedImageBlockWithLightbox;
+
+const BLOCK_NAME = 'core/post-featured-image';
+
+/**
+ * Filters the Featured Image block to add a caption on the singular template.
+ *
+ * @param string|mixed                              $block_content The block content.
+ * @param array{ attrs: array{ isLink?: boolean } } $attributes The block attributes.
+ * @return string The filtered block content.
+ */
+function filter_featured_image_block( mixed $block_content, array $attributes ): string {
+	// Because other plugins can do bad things.
+	if ( ! is_string( $block_content ) ) {
+		$block_content = '';
+	}
+
+	// Bail if "Link to Post" is enabled.
+	if ( isset( $attributes['attrs']['isLink'] ) && $attributes['attrs']['isLink'] ) {
+		return $block_content;
+	}
+
+	$attachment_id = get_post_thumbnail_id();
+	if ( 0 === (int) $attachment_id ) {
+		return $block_content;
+	}
+
+	// Enqueue the required view script and style from the Image block.
+	wp_enqueue_script_module( '@wordpress/block-library/image/view' );
+	wp_enqueue_style( 'wp-block-image' );
+
+	return block_core_image_render_lightbox(
+		$block_content,
+		array(
+			'attrs' => array(
+				'id'       => $attachment_id,
+				'lightbox' => array(
+					'enabled' => true,
+				),
+			),
+		)
+	);
+}
+add_filter(
+	'render_block_' . BLOCK_NAME,
+	filter_featured_image_block( ... ),
+	15, // After Duotone filter and Duotone styles are applied, which apparently is needed.
+	2
+);
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Featured Image Block with Lightbox ===
+
+Contributors: westonruter
+Tested up to: 6.8
+Stable tag:   0.1.0
+License:      GPLv2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Tags:         block theme, featured image, site editor, lightbox
+
+Automatically enables "Enlarge on click" (lightbox) for the Featured Image block when "Link to Post" is not enabled.
+
+== Description ==
+
+This plugin automatically enables "Enlarge on click" (lightbox) for the Featured Image block when "Link to Post" is not enabled. There are no settings for this plugin.
+
+This is a workaround to implement [gutenberg#57849](https://github.com/WordPress/gutenberg/issues/57849).
+
+== Installation ==
+
+1. Download the plugin [ZIP from GitHub](https://github.com/westonruter/featured-image-block-with-lightbox/archive/refs/heads/main.zip) or if you have a local clone of the repo run `npm run plugin-zip`.
+2. Visit **Plugins > Add New Plugin** in the WordPress Admin.
+3. Click **Upload Plugin**.
+4. Select the `featured-image-block-with-lightbox.zip` file on your system from step 1 and click **Install Now**.
+5. Click the **Activate Plugin** button.
+
+You may also install and update via [Git Updater](https://git-updater.com/).
+
+== Changelog ==
+
+= 0.1.0 =
+
+* Initial release.
